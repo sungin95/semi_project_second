36217a9afacb84600fac3ca4637f0af46ba229f8
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2022-11-11 15:42:02 +0900

    feat : sotial_login/fix/templates
    - 소셜 로그인 할때 보이는 화면 템플렛 추가
    - 이제 필요에 따라 고치면 됨.

--- config/settings.py
+++ config/settings.py
@@ -72,7 +72,10 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"],
+        "DIRS": [
+            os.path.join(BASE_DIR, "templates"),
+            os.path.join(BASE_DIR, "templates", "accounts"),
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
