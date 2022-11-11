a2ed43b0d4f33082a9fe57d9a0de6360f84aa2f3
nevertheless0404 <nevertheless.hi@gmail.com>
2022-11-11 17:39:53 +0900

    feat : labs/rating
    - 랩스 등급 페이지 업데이트

--- labs/views.py
+++ labs/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def main(request):
-    context = {
-        
-    }
-    return render(request,'labs/main.html', context)
+    context = {}
+    return render(request, "labs/main.html", context)
+
+
+def rating(request):
+    return render(request, "labs/rating.html")
