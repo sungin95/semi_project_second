b31919d386ec3345c15ec9a85888c49304641cfe
Yoonsik-Shin <sys25800994@gmail.com>
2022-11-11 18:08:07 +0900

    fix : products index
    - products 앱의 index 관련 표기 차이로 오류 발생
    - product_index에서 index로 변경함

--- products/urls.py
+++ products/urls.py
@@ -4,6 +4,5 @@ from . import views
 app_name = "products"
 
 urlpatterns = [
-    path("product_index/", views.product_index, name="product_index"),
     path("index/", views.index, name="index"),
 ]
