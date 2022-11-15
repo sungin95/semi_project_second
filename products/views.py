52e5c34368501bd31ab5bc88b52c47d3b590c8cf
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2022-11-15 16:33:20 +0900

    feat : env
    - env 추가

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from dotenv import load_dotenv
+import os
 
 # Create your views here.
 def index(request):
@@ -6,4 +8,9 @@ def index(request):
 
 
 def detail(request):
-    return render(request, "products/product_detail.html")
+    load_dotenv()
+    KAKAO_KEY = os.getenv("KAKAOKEY")
+    context = {
+        "KAKAO_KEY": KAKAO_KEY,
+    }
+    return render(request, "products/product_detail.html", context)
