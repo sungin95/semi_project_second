31bce0d54804510df48a8a8d9a8362c8da4a7797
nevertheless0404 <nevertheless.hi@gmail.com>
2022-11-15 17:15:13 +0900

    feat : pagination add
    - 커뮤니티 페이지에 페이지네이션 추가

--- communities/views.py
+++ communities/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Articles
+from django.core.paginator import Paginator
 
 
 # Create your views here.
 def index(request):
-    return render(request, "communities/index.html")
+    k = Articles.objects.all().order_by("id")
+    page = request.GET.get("page", "1")
+    paginator = Paginator(k, 3)
+    page_obj = paginator.get_page(page)
+    context = {
+        "v": k,
+        "question_list": page_obj,
+    }
+    return render(request, "communities/index.html", context)
 
 
 def detail(request):
