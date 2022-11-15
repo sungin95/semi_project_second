858772934d7d16ceb3b3eab481a6b523b20d1a2b
Yoonsik-Shin <sys25800994@gmail.com>
2022-11-15 17:10:01 +0900

    feat : category path
    - 네비게이션바 카테고리별 이동 구현
    - 랩스 메인페이지 카테고리별 이동 구현

--- products/views.py
+++ products/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Products
 
 # Create your views here.
 def index(request):
-    return render(request, "products/index.html")
-
+    products = Products.objects.all()
+    category = request.GET.get("category")
+    if category:
+        products_category = Products.objects.filter(제조회사__contains=category)
+    else:
+        products_category = products
+    context = {
+        'products': products,
+        'category': category,
+        'products_category': products_category,
+    }
+    return render(request, "products/index.html", context)
 
 def detail(request):
     return render(request, "products/product_detail.html")
