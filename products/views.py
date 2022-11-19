20253bd47d459e9abeef35abd8d9f0b3774ff7fb
Yoonsik-Shin <sys25800994@gmail.com>
2022-11-19 23:50:36 +0900

    feat : products index filter
    - 인덱스 필터링 기능 정비

--- products/views.py
+++ products/views.py
@@ -6,16 +6,201 @@ from .models import Products
 from .forms import ReviewForm
 from django.http import JsonResponse
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 # Create your views here.
 def index(request):
     products = Products.objects.all()
-
-    products_high = Products.objects.all().order_by("가격")
-    products_low = Products.objects.all().order_by("-가격")
     for product in products:
         product.ten_price = int(round((product.가격) * 1.1))
         product.save()
+
+    # 카테고리 분류
+    print(request.GET.keys())
+    category = request.GET.get("category")
+    if category:
+        products_category = Products.objects.filter(Q(제조회사__contains=category))
+    else:
+        products_category = products
+    # 가격 분류  1,2,3,4,5
+    price = request.GET.get("price")
+    if price:
+        products_price = Products.objects.filter(Q(가격등급__contains=price))
+    else:
+        products_price = products
+    # 무게 분류  1,2,3,4
+    weight = request.GET.get("weight")
+    if weight:
+        products_weight = Products.objects.filter(Q(무게등급__contains=weight))
+    else:
+        products_weight = products
+    # 프로세스 분류 AMD, INTEL, 애플
+    processor = request.GET.get("processor")
+    if processor:
+        products_processor = Products.objects.filter(Q(CPU제조사분류__contains=processor))
+    else:
+        products_processor = products
+    # 프로세스 분류 4000, 5000, 6000, i3, i5, i7, i9, 기타, m1, m2
+    processor_number = request.GET.get("processor-nunber")
+    if processor_number:
+        products_processor_nunber = Products.objects.filter(
+            Q(CPU넘버분류__contains=processor_number)
+        )
+    else:
+        products_processor_nunber = products
+    # 저장용량등급 분류  1,2,3,4
+    storage = request.GET.get("storage")
+    if storage:
+        products_storage = Products.objects.filter(Q(저장용량등급__contains=storage))
+    else:
+        products_storage = products
+    # GPU종류등급 분류  1,2
+    graphic = request.GET.get("graphic")
+    if graphic:
+        products_graphic = Products.objects.filter(Q(GPU종류등급__contains=graphic))
+    else:
+        products_graphic = products
+    # 해상도등급 분류  "FHD","QHD","UHD"
+    resolution = request.GET.get("resolution")
+    if resolution:
+        products_resolution = Products.objects.filter(Q(해상도등급__contains=resolution))
+    else:
+        products_resolution = products
+    # 화면크기등급 분류  1,2,3
+    size = request.GET.get("size")
+    if size:
+        products_size = Products.objects.filter(Q(화면크기등급__contains=size))
+    else:
+        products_size = products
+
+    result = products_category&products_price&products_weight&products_processor&products_processor_nunber&products_storage&products_graphic&products_resolution&products_size
+    print(result)
+    print(len(result))
+    products_category = result
+
+    page = request.GET.get("page", "1")
+    paginator = Paginator(products_category, 6)
+    page_obj = paginator.get_page(result)
+    context = {
+        "products": products,
+        "category": category,
+        "products_category": products_category,
+        "page_obj": page_obj,
+    }
+    return render(request, "products/index.html", context)
+
+
+def detail(request, pk):
+    load_dotenv()
+    KAKAO_KEY = os.getenv("KAKAOKEY")
+    product = get_object_or_404(Products, pk=pk)
+    reviews = product.review_set.all()
+    review_form = ReviewForm()
+    product_grade = 0
+    lenReviews = len(reviews)
+    for review in reviews:
+        product_grade += review.grade
+    if lenReviews != 0:
+        product_grade = round((product_grade / lenReviews), 1)
+    model_name = product.모델명
+    special_price = product.가격
+    price = int(round((product.가격) * 1.1, -4))
+    thumbnail = product.썸네일
+    image1 = product.이미지1
+    image2 = product.이미지2
+    image3 = product.이미지3
+
+    context = {
+        "review_form": review_form,
+        "reviews": reviews,
+        "product_grade": product_grade,
+        "lenReviews": lenReviews,
+        "KAKAO_KEY": KAKAO_KEY,
+        "price": price,
+        "special_price": special_price,
+        "modelName": model_name,
+        "product": product,
+        "thumbnail": thumbnail,
+        "image1": image1,
+        "image2": image2,
+        "image3": image3,
+    }
+    return render(request, "products/detail.html", context)
+
+
+def like_product(request, pk):
+    product = get_object_or_404(Products, pk=pk)
+    if product.like_product.filter(pk=request.user.pk).exists():
+        product.like_product.remove(request.user)
+        is_like = False
+    else:
+        product.like_product.add(request.user)
+        is_like = True
+    context = {
+        "isLiked": is_like,
+        "likeCount": product.like_product.count(),
+    }
+    return JsonResponse(context)
+
+
+def like_reviews(request, product_pk, review_pk):
+    product = get_object_or_404(Products, pk=product_pk)
+    review = get_object_or_404(Review, pk=review_pk)
+    if review.like.filter(pk=request.user.pk).exists():
+        review.like.remove(request.user)
+        is_like = False
+    else:
+        review.like.add(request.user)
+        is_like = True
+    context = {
+        "isLiked": is_like,
+        "likeReviewCount": review.like.count(),
+    }
+    return JsonResponse(context)
+
+
+def review_create(request, pk):
+    product = get_object_or_404(Products, pk=pk)
+    if request.method == "POST":
+        review_form = ReviewForm(request.POST)
+        if review_form.is_valid():
+            review = review_form.save(commit=False)
+            review.user = request.user
+            review.products = product
+            review.save()
+        return redirect("products:detail", product.pk)
+    else:
+        review_form = ReviewForm()
+    context = {
+        "review_form": review_form,
+        "product": product,
+    }
+    return render(request, "products/create.html", context)
+
+
+def update(request, product_pk, review_pk):
+    review = Review.objects.get(pk=review_pk)
+    if request.method == "POST":
+        review_form = ReviewForm(request.POST, instance=review)
+        if review_form.is_valid():
+            review_form.save()
+            return redirect("products:detail", product_pk)
+    else:
+        reviewContent = review.content
+    context = {
+        "reviewContent": reviewContent,
+    }
+    return JsonResponse(context)
+
+
+def delete(request, product_pk, review_pk):
+    get_object_or_404(Review, pk=review_pk).delete()
+    context = {}
+    return JsonResponse(context)
+
+
+def calculate(request):
+    products = Products.objects.all()
     # 가격 등급 나누기.
     for product in products:
         if product.가격 <= 500000:
@@ -139,226 +324,4 @@ def index(request):
         ):
             product.화면크기등급 = "3"
         product.save()
-    # 카테고리 분류
-    category = request.GET.get("category")
-    if category:
-        products_category = Products.objects.filter(제조회사__contains=category)
-    else:
-        products_category = products
-    # 가격 분류  1,2,3,4,5
-    price = request.GET.get("price")
-    if price:
-        products_price = Products.objects.filter(가격등급__contains=price)
-    else:
-        products_price = products
-    # 무게 분류  1,2,3,4
-    weight = request.GET.get("weight")
-    if weight:
-        products_weight = Products.objects.filter(무게등급__contains=weight)
-    else:
-        products_weight = products
-    # 프로세스 분류 AMD, INTEL, 애플
-    processor = request.GET.get("processor")
-    if processor:
-        products_processor = Products.objects.filter(CPU제조사분류__contains=processor)
-    else:
-        products_processor = products
-    # 프로세스 분류 4000, 5000, 6000, i3, i5, i7, i9, 기타, m1, m2
-    processor_number = request.GET.get("processor-nunber")
-    if processor_number:
-        products_processor_nunber = Products.objects.filter(
-            CPU넘버분류__contains=processor_number
-        )
-    else:
-        products_processor_nunber = products
-    # 저장용량등급 분류  1,2,3,4
-    storage = request.GET.get("storage")
-    if storage:
-        products_storage = Products.objects.filter(저장용량등급__contains=storage)
-    else:
-        products_storage = products
-    # GPU종류등급 분류  1,2
-    graphic = request.GET.get("graphic")
-    if graphic:
-        products_graphic = Products.objects.filter(GPU종류등급__contains=graphic)
-    else:
-        products_graphic = products
-    # 해상도등급 분류  "FHD","QHD","UHD"
-    resolution = request.GET.get("resolution")
-    if resolution:
-        products_resolution = Products.objects.filter(해상도등급__contains=resolution)
-    else:
-        products_resolution = products
-    # 화면크기등급 분류  1,2,3
-    size = request.GET.get("size")
-    if size:
-        products_size = Products.objects.filter(화면크기등급__contains=size)
-    else:
-        products_size = products
-
-    # 합치는 작업
-    a = set()
-    for j in products_category:
-        a.add(j.pk)
-    b = set()
-    for i in products_price:
-        b.add(i.pk)
-    c = set()
-    for i in products_weight:
-        c.add(i.pk)
-    d = set()
-    for i in products_processor:
-        d.add(i.pk)
-    d_num = set()
-    for i in products_processor_nunber:
-        d_num.add(i.pk)
-    # e = set()
-    # for i in products_storage:
-    #     e.add(i.pk)
-    f = set()
-    for i in products_graphic:
-        f.add(i.pk)
-    g = set()
-    for i in products_resolution:
-        g.add(i.pk)
-    h = set()
-    for i in products_size:
-        h.add(i.pk)
-
-    # if category:
-    # if price:
-    # if weight:
-    # if processor:
-    # if processor_number:
-    # if storage:
-    # if graphic:
-    # if resolution:
-    # if size:
-
-    sum = a & b & c & d & f & g & h & d_num  # & e
-
-    answer = []
-    for p in products:
-        if p.pk in sum:
-            answer.append(p)
-    page = request.GET.get("page", "1")
-    paginator = Paginator(products_category, 6)
-    page_obj = paginator.get_page(page)
-    context = {
-        "products": products,
-        "category": category,
-        "products_high": products_high,
-        "products_low": products_low,
-        "products_category": products_category,
-        "page_obj": page_obj,
-    }
-    return render(request, "products/index.html", context)
-
-
-def detail(request, pk):
-    load_dotenv()
-    KAKAO_KEY = os.getenv("KAKAOKEY")
-    product = get_object_or_404(Products, pk=pk)
-    reviews = product.review_set.all()
-    review_form = ReviewForm()
-    product_grade = 0
-    lenReviews = len(reviews)
-    for review in reviews:
-        product_grade += review.grade
-    if lenReviews != 0:
-        product_grade = round((product_grade / lenReviews), 1)
-    model_name = product.모델명
-    special_price = product.가격
-    price = int(round((product.가격) * 1.1, -4))
-    thumbnail = product.썸네일
-    image1 = product.이미지1
-    image2 = product.이미지2
-    image3 = product.이미지3
-
-    context = {
-        "review_form": review_form,
-        "reviews": reviews,
-        "product_grade": product_grade,
-        "lenReviews": lenReviews,
-        "KAKAO_KEY": KAKAO_KEY,
-        "price": price,
-        "special_price": special_price,
-        "modelName": model_name,
-        "product": product,
-        "thumbnail": thumbnail,
-        "image1": image1,
-        "image2": image2,
-        "image3": image3,
-    }
-    return render(request, "products/detail.html", context)
-
-
-def like_product(request, pk):
-    product = get_object_or_404(Products, pk=pk)
-    if product.like_product.filter(pk=request.user.pk).exists():
-        product.like_product.remove(request.user)
-        is_like = False
-    else:
-        product.like_product.add(request.user)
-        is_like = True
-    context = {
-        "isLiked": is_like,
-        "likeCount": product.like_product.count(),
-    }
-    return JsonResponse(context)
-
-
-def like_reviews(request, product_pk, review_pk):
-    product = get_object_or_404(Products, pk=product_pk)
-    review = get_object_or_404(Review, pk=review_pk)
-    if review.like.filter(pk=request.user.pk).exists():
-        review.like.remove(request.user)
-        is_like = False
-    else:
-        review.like.add(request.user)
-        is_like = True
-    context = {
-        "isLiked": is_like,
-        "likeReviewCount": review.like.count(),
-    }
-    return JsonResponse(context)
-
-
-def review_create(request, pk):
-    product = get_object_or_404(Products, pk=pk)
-    if request.method == "POST":
-        review_form = ReviewForm(request.POST)
-        if review_form.is_valid():
-            review = review_form.save(commit=False)
-            review.user = request.user
-            review.products = product
-            review.save()
-        return redirect("products:detail", product.pk)
-    else:
-        review_form = ReviewForm()
-    context = {
-        "review_form": review_form,
-        "product": product,
-    }
-    return render(request, "products/create.html", context)
-
-
-def update(request, product_pk, review_pk):
-    review = Review.objects.get(pk=review_pk)
-    if request.method == "POST":
-        review_form = ReviewForm(request.POST, instance=review)
-        if review_form.is_valid():
-            review_form.save()
-            return redirect("products:detail", product_pk)
-    else:
-        reviewContent = review.content
-    context = {
-        "reviewContent": reviewContent,
-    }
-    return JsonResponse(context)
-
-
-def delete(request, product_pk, review_pk):
-    get_object_or_404(Review, pk=review_pk).delete()
-    context = {}
-    return JsonResponse(context)
+    return redirect("products:index")
