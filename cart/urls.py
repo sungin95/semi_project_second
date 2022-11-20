7e02efec22e536f398f8f675d5f6ea925f694e2e
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2022-11-21 00:22:47 +0900

    feat : cart/point
    - cart 포인트에 따른 가격 변화

--- cart/urls.py
+++ cart/urls.py
@@ -5,6 +5,7 @@ app_name = "cart"
 
 urlpatterns = [
     path("", views.detail, name="detail"),
+    path("user_point/<int:value>", views.user_point, name="user_point"),
     path("add/<int:product_id>/", views.add_cart, name="add_cart"),
     path("remove/<int:product_id>/", views.cart_remove, name="cart_remove"),
     path("full_remove/", views.full_remove, name="full_remove"),
