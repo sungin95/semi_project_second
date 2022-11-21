8f656d492778be6f42624e4c1fc5f493a4c8ebe9
Yoonsik-Shin <sys25800994@gmail.com>
2022-11-22 02:03:10 +0900

    refact : grade page refact

--- labs/static/labs/js/rating.js
+++ labs/static/labs/js/rating.js
@@ -1,68 +1,62 @@
-$(function() {
-  $("#test1").on("click", function() {
-      $("#first").show();
-  })
-  $("#test1").on("click", function() {
-      $("#second").hide();
-  })
-})
-
-$(function() {
-  $("#test1").on("click", function() {
-      $("#first").show();
-  })
-  $("#test1").on("click", function() {
-      $("#third").hide();
-  })
-})
-
-
-$(function() {
-  $("#test2").on("click", function() {
-      $("#second").show();
-  })
-  $("#test2").on("click", function() {
-      $("#first").hide();
-  })
-})
-
-$(function() {
-  $("#test2").on("click", function() {
-      $("#second").show();
-  })
-  $("#test2").on("click", function() {
-      $("#third").hide();
-  })
-})
-
-
-$(function() {
-  $("#test3").on("click", function() {
-      $("#third").show();
-  })
-  $("#test3").on("click", function() {
-      $("#second").hide();
-  })
-})
-
-$(function() {
-  $("#test3").on("click", function() {
-      $("#third").show();
-  })
-  $("#test3").on("click", function() {
-      $("#first").hide();
-  })
-})
-
-
-$(document).ready(function(){
-  $("#test1").trigger('click'); 
+$(function () {
+    $("#test1").on("click", function () {
+        $("#first").show();
+    });
+    $("#test1").on("click", function () {
+        $("#second").hide();
+    });
 });
 
+$(function () {
+    $("#test1").on("click", function () {
+        $("#first").show();
+    });
+    $("#test1").on("click", function () {
+        $("#third").hide();
+    });
+});
 
+$(function () {
+    $("#test2").on("click", function () {
+        $("#second").show();
+    });
+    $("#test2").on("click", function () {
+        $("#first").hide();
+    });
+});
+
+$(function () {
+    $("#test2").on("click", function () {
+        $("#second").show();
+    });
+    $("#test2").on("click", function () {
+        $("#third").hide();
+    });
+});
+
+$(function () {
+    $("#test3").on("click", function () {
+        $("#third").show();
+    });
+    $("#test3").on("click", function () {
+        $("#second").hide();
+    });
+});
+
+$(function () {
+    $("#test3").on("click", function () {
+        $("#third").show();
+    });
+    $("#test3").on("click", function () {
+        $("#first").hide();
+    });
+});
+
+$(document).ready(function () {
+    $("#test1").trigger("click");
+});
 
 var focused = null;
 $(".test1").focus(function () {
     focused = $(this);
 });
-
